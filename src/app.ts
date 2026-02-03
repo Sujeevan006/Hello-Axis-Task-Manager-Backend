fd2956f72bcc233316df367142e285242238a116
Sujeevan <sujeevan0024@gmail.com>
2026-02-03 20:26:38 +0530

    feat: Initialize Express application with core middleware, API routes, and update cors dependency.

--- src/app.ts
+++ src/app.ts
@@ -12,12 +12,19 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ['https://project.axivers.com', 'http://localhost:3000'],
+    origin: [
+      'https://project.axivers.com', // your frontend domain
+      'http://localhost:3000', // optional (for local dev)
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
+
+// VERY IMPORTANT
+app.options('*', cors());
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
